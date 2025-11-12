26f668bfece80be7ab34c643f4c5c58a551d981b
Christina Hedges <christina.l.hedges@nasa.gov>
2025-11-12 14:28:17 -0500

    last one

--- src/sparse3d/sparse3d.py
+++ src/sparse3d/sparse3d.py
@@ -159,7 +159,7 @@ class Sparse3D(Sparse3DMathMixin, sparse.coo_matrix):
         return self.__class__(
             data=new_data,
             row=new_row + self.imcorner[0],
-            col=new_col + self.imcorner[0],
+            col=new_col + self.imcorner[1],
             imshape=self.imshape,
             imcorner=self.imcorner,
         )
